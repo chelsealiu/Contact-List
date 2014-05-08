7ff611ad45cf5da77e886530deee0d7d983d9f8a
Aaron Dufall <aald212@gmail.com>
2014-05-07 18:20:22 -0700

    Initial Commit

---
+++ ContactList/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Aaron Dufall on 7/05/2014.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
